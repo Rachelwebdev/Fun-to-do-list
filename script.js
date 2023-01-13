0dbd5a040a4992d0b735e84258a27b66665ac271
Rachel Isaac <rachelisaac995@gmail.com>
2023-01-13 23:48:54 +0400

    Get the current date and insert in inner HTML

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const listContainer = document.querySelector(".display-list");
 const listTasks = document.querySelector(".item");
 
 const dateToday = document.querySelector(".date");
+
+// Display Current date
+const date = new Date().toLocaleDateString();
+dateToday.innerHTML = date;
